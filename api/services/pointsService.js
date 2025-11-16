3d5ad74f87898c518ab9483243cf5f8f3949f776
etienneperche <e.perche59@gmail.com>
2025-11-16 15:39:02 +0100

    update onFlight fetching points

--- api/services/pointsService.js
+++ api/services/pointsService.js
@@ -1,12 +1,7 @@
 const axios = require('axios');
 const { 
   fetchVentualsPoints,
-  fetchFelixPoints,
-  fetchGliquidPoints,
-  fetchHyperbeatPoints,
-  fetchHyperlendPoints,
   fetchHybraPoints,
-  fetchHypurrfiPoints,
   fetchPrjxPoints,
   fetchUltrasolidPoints
 } = require('./platforms');
@@ -19,12 +14,7 @@ const {
 async function fetchAllPoints(address) {
   const results = {
     ventuals: null,
-    felix: null,
-    gliquid: null,
-    hyperbeat: null,
-    hyperlend: null,
     hybra: null,
-    hypurrfi: null,
     prjx: null,
     ultrasolid: null
   };
@@ -32,35 +22,20 @@ async function fetchAllPoints(address) {
   // Fetch all points in parallel
   const promises = [
     fetchVentualsPoints(address).catch(err => ({ error: err.message })),
-    fetchFelixPoints(address).catch(err => ({ error: err.message })),
-    fetchGliquidPoints(address).catch(err => ({ error: err.message })),
-    fetchHyperbeatPoints(address).catch(err => ({ error: err.message })),
-    fetchHyperlendPoints(address).catch(err => ({ error: err.message })),
     fetchHybraPoints(address).catch(err => ({ error: err.message })),
-    fetchHypurrfiPoints(address).catch(err => ({ error: err.message })),
     fetchPrjxPoints(address).catch(err => ({ error: err.message })),
     fetchUltrasolidPoints(address).catch(err => ({ error: err.message }))
   ];
 
   const [
     ventuals,
-    felix,
-    gliquid,
-    hyperbeat,
-    hyperlend,
     hybra,
-    hypurrfi,
     prjx,
     ultrasolid
   ] = await Promise.all(promises);
 
   results.ventuals = ventuals;
-  results.felix = felix;
-  results.gliquid = gliquid;
-  results.hyperbeat = hyperbeat;
-  results.hyperlend = hyperlend;
   results.hybra = hybra;
-  results.hypurrfi = hypurrfi;
   results.prjx = prjx;
   results.ultrasolid = ultrasolid;
 
